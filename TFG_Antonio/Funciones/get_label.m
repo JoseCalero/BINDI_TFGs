a410bee7501eafae7d342b02bdbd94b2800dd787
Antonio Aranzana <100345920@alumnos.uc3m.es>
2020-04-17 17:18:50 +0200

    RQA

--- TFG_Antonio/Funciones/get_label.m
+++ TFG_Antonio/Funciones/get_label.m
@@ -1,31 +1,37 @@
 %% FUNCION QUE RECOGE EL ETIQUETADO EN VALENCE-AROUSAL DEL SELF REPORT
 %
 
-function label_out = get_label(k, ventanas, VA, baseline, stress, medi1, amusement, medi2)
+function [label_out_VA, label_out_estado] = get_label(k, ventanas, VA, baseline, stress, medi1, amusement, medi2)
 %% LE DAMOS EL VALOR A LABEL_OUT 
 %
 %
 
-label_out = 0;
+label_out_VA = 0;
+label_out_estado = 0;
 
-if baseline(1)*64/700 <= ventanas(k) && baseline(end)*64/700 >= ventanas(k)
-    label_out = VA(1);
+if round(baseline(1)*64/700) <= ventanas(k) && round(baseline(end)*64/700) >= ventanas(k)
+    label_out_VA = VA(1);
+    label_out_estado = 1;
 end
     
-if stress(1)*64/700 <= ventanas(k) && stress(end)*64/700 >= ventanas(k)
-    label_out = VA(2);
+if round(stress(1)*64/700) <= ventanas(k) && round(stress(end)*64/700) >= ventanas(k)
+    label_out_VA = VA(2);
+    label_out_estado = 2;
 end
 
-if medi1(1)*64/700 <= ventanas(k) && medi1(end)*64/700 >= ventanas(k)
-    label_out = VA(3);
+if round(medi1(1)*64/700) <= ventanas(k) && round(medi1(end)*64/700) >= ventanas(k)
+    label_out_VA = VA(3);
+    label_out_estado = 3;
 end
 
-if amusement(1)*64/700 <= ventanas(k) && amusement(end)*64/700 >= ventanas(k)
-    label_out = VA(4);
+if round(amusement(1)*64/700) <= ventanas(k) && round(amusement(end)*64/700) >= ventanas(k)
+    label_out_VA = VA(4);
+    label_out_estado = 4;
 end
 
-if medi2(1)*64/700 <= ventanas(k) && medi2(end)*64/700 >= ventanas(k)
-    label_out = VA(5);    
+if round(medi2(1)*64/700) <= ventanas(k) && round(medi2(end)*64/700) >= ventanas(k)
+    label_out_VA = VA(5);    
+    label_out_estado = 5;
 end
 
 end
