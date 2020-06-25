bba638eff8f0dcd7948e461c278724db76900c83
Antonio Aranzana <100345920@alumnos.uc3m.es>
2020-06-25 16:33:33 +0200

    TINN
    Se estaba trabajando con IBIs en vez de valores de HRV

--- TFG_Antonio/Funciones/TINN_funct.m
+++ TFG_Antonio/Funciones/TINN_funct.m
@@ -4,10 +4,12 @@ function TINN = TINN_funct(pulsos_totales, numero_de_pulso)
     
 i = 2:pulsos_totales;
 
-z = 1;
 for i = 2:pulsos_totales
-    matriz_HRV1(z) = (1/64)*numero_de_pulso(i)-(1/64)*numero_de_pulso(i-1);
-    z = z + 1;
+    IBIs(i-1) = (1/64)*numero_de_pulso(i) - (1/64)*numero_de_pulso(i-1);
+end
+
+for i = 2:length(IBIs)
+    matriz_HRV1(i-1) = abs(IBIs(i) - IBIs(i-1));
 end
 
 matriz_HRV = matriz_HRV1.*100; % Aproximación al segundo decimal
@@ -40,37 +42,24 @@ for n = min_HRV:max_HRV
     end
 end
 
+% Minimum square difference para hallar el triángulo - las ventanas de 16
+% segundos siempre tienen valores mínimos de 1 repetición, por lo que éste
+% ha sido el corte en nuestra función para hallar el triángulo
+
 F = 2;
-%C = 1;
-%a = 1;
-%triangulo_encontrado_tiempos(F; C) = 0;
 triangulo_encontrado = 0;
 
 for n = 2:length(vector_repeticiones)
     if (vector_repeticiones(n) ~= 1) 
-        %triangulo_encontrado_tiempos(F;C) = tiempos(n);
-        %triangulo_encontrado_repes(F;C) = vector_repeticiones(n);
         triangulo_encontrado(F-1) = tiempos(n-1);
         triangulo_encontrado(F) = tiempos(n);
         F = F + 1;
         if n < length(tiempos)
             triangulo_encontrado(F+1) = tiempos(n+1);
         end
-     %elseif (vector_repeticiones(n) ~= 1) && (a ~= length(triangulo_encontrado_tiempos(1)))
-     %   C = C + 1;
-      %  F = 1;
-       % triangulo_encontrado_tiempos(F;C) = tiempos(n);
-        %triangulo_encontrado_repes(F;C) = vector_repeticiones(n);
-        %F =  F + 1;
-    %elseif (vector_repeticiones(n)) == 1 && (aux == tiempos(n))
-        %triangulo_encontrado(F) = tiempos(n);
-        %n = length(vector_repeticiones);
     end
 end
 
 TINN = (triangulo_encontrado(end)-triangulo_encontrado(1))*10; % En ms
-%if (TINN == 0)
-%    TINN = 10; % Mínimo valor de TINN
-%end
 
 end
